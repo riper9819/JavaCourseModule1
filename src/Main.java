97b4039163a3030906626f549c4ac2e6b72620e6
riper9819 <riper9819@mail.ru>
2020-05-29 21:29:09 +0300

    Updates-1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 
 class Main {
     public static void main(String[] args) throws Exception {
-        String text = new String (Files.readAllBytes(Paths.get("HarryPotter.txt")));
+        //Creating
+        String text = new String (Files.readAllBytes(Paths.get("HarryPotter.txt")),StandardCharsets.UTF_8);
         text = text.replaceAll("\n", " ").
                 replaceAll("[.%,;?!@:№\"[0-9]]", "").
-                replaceAll("- ", "").
-                replaceAll("  ", " ");
+                replaceAll("- ", "");
 
+        //removing incorect spaces
+        text = text.replaceAll("\\s+", " ");
         String[] words = text.split(" ");
         String longest = "";
         for (int i = 0; i < words.length; i++){
@@ -18,8 +22,9 @@ class Main {
             }
         }
 
-        List <String> linesArray = Files.readAllLines(Paths.get("HarryPotter.txt"));
+        //Creating List and counting number of word "Гаррі" repetitions in text
 
+        List <String> linesArray = Files.readAllLines(Paths.get("HarryPotter.txt"));
         int counter = 0;
         for (int i = 0; i < linesArray.size(); i++){
             if (linesArray.get(i).contains("Гаррі")){
@@ -27,21 +32,41 @@ class Main {
             }
         }
 
-        String [] hashCodes = new String [linesArray.size()];
-        for (int i = 0; i < linesArray.size(); i++){
-            hashCodes[i] = String.valueOf(linesArray.get(i).hashCode());
-            System.out.println("Line number " + (i+1) + " has hash code: " + hashCodes[i]);
-        }
+        //Creating Array of text distinct words
 
-        int intersections = 0;
-        for (int i = 0; i < hashCodes.length; i++){
-            for (int j = 0; j < hashCodes.length; j++){
-                if (hashCodes[i] == hashCodes[j] && i != j){
-                    intersections++;
-                }
+        String distincts = "";
+        for (int i = 0; i < words.length; i++){
+            if(!distincts.contains(words[i])){
+                distincts += words[i] + " ";
             }
         }
-        System.out.println("There are " + intersections + " intersections in hash!");
+        String[] distinctArray = distincts.split(" ");
+        Arrays.sort(distinctArray);
+
+        //Hashing all text words in Array
+
+        int [] hashCodes = new int [words.length];
+        for (int i = 0; i < words.length; i++){
+            hashCodes[i] = (words[i]).hashCode();
+            System.out.println(words[i] + "     has hash code: " + hashCodes[i]);
+        }
+
+        //Counting hash intersections
+
+        int intersections = 0;
+        String [] intersectionArray = new String [distinctArray.length];
+        for (int i = 0; i < hashCodes.length; i++){
+            if(Arrays.asList(intersectionArray).contains(String.valueOf(hashCodes[i]))){
+                System.out.println(intersections);
+                intersectionArray[intersections] = String.valueOf(hashCodes[i]);
+                intersections ++;
+            }
+        }
+
+        //Results of calculations
+        System.out.println("\n**************Results of calculations**************\n");
+        System.out.println("There are " + intersections + " intersections in words hash.");
+        System.out.println("There are " + distincts.length() + " distinct words in text.");
         System.out.println("There are " + counter + " words \"Гаррі\" in text.");
         System.out.println("Longest word is: " + longest);
     }
