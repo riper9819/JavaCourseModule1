40e82eb9c229ac6ba738337a82f6fbde83957f5c
riper9819 <riper9819@mail.ru>
2020-06-21 16:27:46 +0300

    Finished task

--- src/Module3/Main.java
+++ src/Module3/Main.java
@@ -10,7 +10,7 @@
  * Description:
     JavaCollectionsFramework. List.
     1. Create ArrayList and LinkedList containing  100 000 Integer elements. Compare time intervals.
-    2. Insert  new 1000 elements  on the  beginning, on  the middle  and on the end of ones. Compare time intervals.
+    2. Insert new 1000 elements  on the  beginning, on  the middle  and on the end of ones. Compare time intervals.
     3. Update 1000 elements  from the  beginning, from  the middle  and from the end of ones. Compare time intervals.
     4. Delete 1000 elements  from the  beginning, from  the middle  and from the end of ones. Compare time intervals.
  */
